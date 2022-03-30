bc7350ab1c4b9d7eacf2eb9d5b679b8a303b3849
Tomas Ortega <tomasvortegar@gmail.com>
2022-03-30 03:48:32 -0400

    adding env script

---
+++ create_venv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# activate existed python3 module to get virtualenv
+module load python3/intel/3.6.3
+
+# create virtual environment with python3
+virtualenv -p python3 .env
+
+# activate virtual environment
+source .env/bin/activate
+
+# install pytorch and orther stuff
+pip install --upgrade pip
+pip install tensorflow-gpu
+pip install torch
+pip install keras 
+pip install torchvision 
+pip install jupyter
+# uncomment the following line if you have requirements.txt file
+# pip install -r requirements.txt
+
+# unload module
+module unload python3/intel/3.8.6
