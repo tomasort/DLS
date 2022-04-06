7b8caf001e4d55393fa2af2a7ac200742f550af4
Tomas Ortega <tomasvortegar@gmail.com>
2022-04-06 01:43:23 -0400

    question 2 is done?

--- training_script_p2.py
+++ training_script_p2.py
@@ -20,14 +20,15 @@ transform = transforms.Compose(
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 
-path = "/scratch/tor213/"
+data_path = "/scratch/tor213/data"
+path = "/scratch/tor213/DLS"
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-trainset = torchvision.datasets.CIFAR10(root=path + 'data', train=True,
+trainset = torchvision.datasets.CIFAR10(root=data_path, train=True,
                                     download=True, transform=transform)
-testset = torchvision.datasets.CIFAR10(root=path + 'data', train=False,
+testset = torchvision.datasets.CIFAR10(root=data_path, train=False,
                                     download=True, transform=transform)
 
 class AverageMeter(object):
@@ -304,7 +305,7 @@ def ResNet18():
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 torch.manual_seed(0)
 
-max_batch_size = 2**15
+max_batch_size = 2**22
 print(f"Starting training with {torch.cuda.device_count()} GPUs")
 b = 32  # Initial batch size
 num_epochs = 2  # The first epoch is to bring things to the cache and stuff
